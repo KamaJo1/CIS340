8f0903b92921b37442649ab0d481ec57feeaad29
KamaJo1 <93611462+KamaJo1@users.noreply.github.com>
2022-04-26 18:20:56 +0000

    Update App.js

--- MyFourthProject/App.js
+++ MyFourthProject/App.js
@@ -1,32 +1,56 @@
 import React from 'react';
-import { Text,View, SectionList } from 'react-native';
+import {Text, View, SectionList} from 'react-native';
 
-export default  StatesApp = () => {
+export default StatesApp = () => {
 
-  return (
+    return (
+        <View style={
+            {
+                flex: 1,
+                paddingTop: 22
+            }
+        }>
+            <SectionList  sections={
+                    [
+                        {
+                            title: 'A',
+                            data: ['Alabama, Alaska', 'Arazona', 'Arkansas']
+                        },
+                        {
+                            title: 'C',
+                            data: ['Californa', 'Colorado', 'Connecticut']
+                        },
+                        {
+                            title: 'D',
+                            data: ['Delware']
+                        },
+                        {
+                            title: 'F',
+                            data: ['Florida']
+                        }, {
+                            title: 'G',
+                            data: ['Georga']
+                        }, {
+                            title: 'H',
+                            data: ['Hawaii']
+                        },
 
-    <View style = {{flex: 1, paddingTop: 22}}>
-      <SectionList 
-      sections={[
-      {title: 'A', data: ['Alabama, Alaska', 'Arazona','Arkansas']},
-      {title: 'C', data: ['Californa', 'Colorado', 'Connecticut']},
-      {title: 'D', data: ['Delware']},
-      {title: 'F', data: ['Florida']},
-      {title: 'G', data: ['Georga'] },
-      {title: 'H', data: ['Hawaii']},
 
-
-      ]}//i can continue with more sections
-      returnItem ={({item}) => <Text style = {{padding:10, fontSize: 20, height:44}}> {item}</Text>}
-      renderSectionHeader = {({section}) => <Text style = {{paddingTop: 6, paddingLeft: 12, paddingRight: 12, paddingBottom: 6, fontSize: 12, fontWeight: 'bold', backgroundColor:'#9FA8DA',}} >
+                    ]
+                }
+                //i can continue with more sections
+                renderItem={({item}) => <Text style = {{padding:10, fontSize: 20, height:44}}> {item}</Text>}
+                renderSectionHeader=
+                {({section}) => <Text style = {{paddingTop: 6, paddingLeft: 12, paddingRight: 12, paddingBottom: 6,
+                 fontSize: 12, fontWeight: 'bold', backgroundColor:'#9FA8DA',}} >
         
          </Text>}
-         keyExtractor = {(item,index) => index}
-        
-         />
-    
-    </View>
-  
-  );
+                keyExtractor=
+                {(item,index) => index}
+            />
+
+        </View>
+
+    );
 
 }
